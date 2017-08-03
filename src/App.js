cff94a332b0debe54128dbd8257102076e95909f
tangweikun <819573105@qq.com>
2017-08-03 21:09:13 +0800

    Call auth when click the login button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Auth from './Auth'
+
+const auth = new Auth()
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    console.log('====', auth.isAuthenticated)
+    return <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,8 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div onClick={() => auth.login()}>Login</div>
+        <div onClick={() => auth.logout()}>Logout</div>
       </div>
-    );
   }
 }
 
---
+++ src/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: "twk.auth0.com",
+  clientId: "FDmxm_w9wm4S0G2vpsDTDZ5sY_R3y_Kg",
+  callbackUrl: "http://localhost:3000"
+};
